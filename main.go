d5ee0772fbea610742dc0860a787a1aa15a06520
Raoul <r.verspaij@student.fontys.nl>
2022-12-12 12:58:41 +0100

    api compleet

--- main.go
+++ main.go
@@ -2,44 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type reservation struct {
-	Username string `json:"inp_username"`
-	Password string `json:"inp_password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 	// LastName	string		`json:"lastname"`
 	// Time		int			`json:"time"`
 }
 
 var reservations = []reservation{
-	{Username: "1", Password: "Admin01"},
+	{Username: "loloke1", Password: "Admin01"},
 }
 
 func addReservation(c *gin.Context) {
 	var newReservation reservation
 
-	version := c.Param("version")
-	fmt.Println("Version", version)
-	if version == "v2" {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type")
-	}
-
 	if err := c.BindJSON(&newReservation); err != nil {
+		log.Fatal(err)
 		return
 	}
-
 	reservations = append(reservations, newReservation)
+	fmt.Println(reservations)
 	c.IndentedJSON(http.StatusCreated, newReservation)
-	fmt.Println(newReservation)
 }
 
 func main() {
 	router := gin.Default()
-	router.SetTrustedProxies(nil)
-	router.POST("/reservation", addReservation)
+	router.POST("/reservations", addReservation)
 	router.Run("localhost:8080")
 }
